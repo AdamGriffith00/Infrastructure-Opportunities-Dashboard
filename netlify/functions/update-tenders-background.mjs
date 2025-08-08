c502c078fe68ea6a1443e05c6f04603dd145754d
AdamGriffith00 <adam_griffith@live.com>
2025-08-08 20:43:08 +0100

    Update update-tenders-background.mjs

--- netlify/functions/update-tenders-background.mjs
+++ netlify/functions/update-tenders-background.mjs
@@ -1,5 +1,4 @@
 import { handler as runNow } from "./update-tenders.mjs";
 export async function handler() {
-  // Returns a response, but Netlify just logs it—fine for our purpose.
   return await runNow();
 }
