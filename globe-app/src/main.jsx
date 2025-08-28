ec13e5ef6428b4170fe017339604fdf723b4b669
AdamGriffith00 <adam_griffith@live.com>
2025-08-28 16:47:50 +0100

    Create main.jsx

---
+++ globe-app/src/main.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import InteractiveGlobe from './modules/InteractiveGlobe.jsx'
+
+createRoot(document.getElementById('root')).render(<InteractiveGlobe />)
